a10e63aa5e8bed50b865092b64ed2e638837fd56
kalessine <jsg@internode.on.net>
2016-08-09 22:48:40 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	build/web/WEB-INF/classes/sdmx/gateway/RepositoryService$1.class
    	build/web/WEB-INF/classes/sdmx/gateway/RepositoryService.class
    	build/web/WEB-INF/lib/sdmx-sax.jar
    	nbproject/private/private.xml
    	src/java/sdmx/gateway/RepositoryService.java
    	web/index.html

--- src/sdmx/querykey/QueryKey.java
+++ src/sdmx/querykey/QueryKey.java
@@ -15,8 +15,10 @@ import sdmx.structure.datastructure.DataStructureType;
  * @author James
  */
 public class QueryKey {
+    private String dataflowId = null;
     List<QueryDimension> dims = new ArrayList<QueryDimension>();
-     public QueryKey(DataStructureType ds,Registry reg){
+     public QueryKey(DataStructureType ds,Registry reg,String dataflowId){
+         this.dataflowId=dataflowId;
          for(int i=0;i<ds.getDataStructureComponents().getDimensionList().size();i++) {
              dims.add(new QueryDimension(ds.getDataStructureComponents().getDimensionList().getDimension(i).getId().toString(),ds,reg));
          }
@@ -40,4 +42,22 @@ public class QueryKey {
      public List<QueryDimension> getQueryDimensions() {
          return this.dims; 
      }
+
+    /**
+     * @return the dataflowId
+     */
+    public String getDataflowId() {
+        return dataflowId;
+    }
+
+    /**
+     * @param dataflowId the dataflowId to set
+     */
+    public void setDataflowId(String dataflowId) {
+        this.dataflowId = dataflowId;
+    }
+    public int size() {
+        return this.dims.size();
+    }
+        
 }
